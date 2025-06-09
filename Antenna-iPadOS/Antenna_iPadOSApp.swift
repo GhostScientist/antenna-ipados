4f0a6165ed338585a7508265a4fd856c76e0b3f8
Dakota Kim <codingvampire@gmail.com>
2025-06-08 21:44:42 -0400

    Initial Commit

---
+++ Antenna-iPadOS/Antenna_iPadOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Antenna_iPadOSApp.swift
+//  Antenna-iPadOS
+//
+//  Created by Dakota Kim on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Antenna_iPadOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
